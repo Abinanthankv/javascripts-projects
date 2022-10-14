13d8a6893577327206880205d3c9bfa671a41903
Abinanthan <abinanthankv@protonmail.com>
2022-10-14 16:13:43 +0530

    update

--- Weather/app.js
+++ Weather/app.js
@@ -1,4 +1,9 @@
 
+function searchcity() {
+
+    city = document.getElementById("search").value;
+    fetchcity(city);
+}
 
 const apikey = "d702660defc3b728234840c245d99fa7";
 let lat;
@@ -21,6 +26,8 @@ let condition = [];
 let intensity = [];
 let humidity;
 let current_date;
+var dailydata;
+var displayweekly=[];
 /* To get day,date,time*/ 
 /*-----------------------------------------------------------------------*/ 
 var current_time;
@@ -38,17 +45,13 @@ else{
     current_time=hour+":"+minutes;
 }
 
-console.log(minutes);
+
 /**--------------------------------------------------------------------------------------------- */
 let city = "Harur";
 
 fetchcity(city);
 
-function searchcity() {
 
-    city = document.getElementById("search").value;
-    fetchcity(city);
-}
 
 
 function fetchcity(city) {
@@ -87,45 +90,76 @@ function doesFileExist(urlToFile)
     if (xhr.status == "404") {
         if(description.includes("clouds"))
         {
-            document.querySelector(".icon1").src="icons/overcast clouds.svg";  
+            document.querySelector(".icon1").src="icons/overcastclouds.svg";  
+            document.querySelector(".mini-icon").src="icons/overcastclouds.svg";
+            
         }
         else if(description.includes("rain"))
         {
             document.querySelector(".icon1").src="icons/rain.svg";
+            document.querySelector(".mini-icon").src="icons/rain.svg";
+            
         }
         else if(description.includes("snow"))
         {
             document.querySelector(".icon1").src="icons/snow.svg";
+            document.querySelector(".mini-icon").src="icons/snow.svg";
+            
         }
         else if(description.includes("hail"))
         {
             document.querySelector(".icon1").src="icons/hail.svg";
+            document.querySelector(".mini-icon").src="icons/hail.svg";
+            
         }
         else if(description.includes("haze"))
         {
             document.querySelector(".icon1").src="icons/haze.svg";
+            document.querySelector(".mini-icon").src="icons/haze.svg";
+            
         }
         else if(description.includes("thunderstorms"))
         {
             document.querySelector(".icon1").src="icons/thunderstorms.svg";
+            document.querySelector(".mini-icon").src="icons/thunderstorms.svg";
+            
         }
         else if(description.includes("fog"))
         {
             document.querySelector(".icon1").src="icons/fog.svg";
+            document.querySelector(".mini-icon").src="icons/fog.svg";
+            
         }
         else if(description.includes("clear"))
         {
-            document.querySelector(".icon1").src="icons/clear sky.svg";
+            document.querySelector(".icon1").src="icons/clearsky.svg";
+            document.querySelector(".mini-icon").src="icons/clearsky.svg";
+            
+        }
+        else if(description.includes("mist"))
+        {
+            document.querySelector(".icon1").src="icons/mist.svg";
+            document.querySelector(".mini-icon").src="icons/mist.svg";
+            
         }
         
-    } else {
-        console.log(document.querySelector('.icon2'));
+        
+        
+    }
+     else {
+        
         document.querySelector(".icon1").src="icons/"+description+".svg";
         document.querySelector(".mini-icon").src="icons/"+description+".svg";
         
         
+        
+        
+        
 
     }
+    
+    
+
 }
 /**----------------------------------------------------------------------------------------------------- */
 
@@ -133,22 +167,16 @@ function displayWeather(data) {
     description = data.current.weather[0].description;
     current_temp = data.current.temp;
     humidity = data.current.humidity;
-    let dailydata = data.daily;
-    let hourlydata=data.hourly;
+    dailydata = data.daily;
+    weeklydata(dailydata);
+  
     doesFileExist("icons/"+description+".svg")
-    console.log(data);
-   
-   for (let i = 0; i <= dailydata.length - 1; i++) {
-        daily_description[i] = dailydata[i].weather[0].description;
-        daily_icon[i] = dailydata[i].weather[0].icon
-        date[i] = new Date(dailydata[i].dt * 1000).toDateString();
-        temp_max[i] = dailydata[i].temp.max;
-        temp_min[i] = dailydata[i].temp.min;
-
-    }
-    current_date = date[0];
     
-    console.log(current_day);
+   
+
+    
+    
+   
     /*----------------------------------------------------------------------------------------*/
     /*To display curent weather condition*/
     /*------------------------------------------------------------------------------------------*/
@@ -156,7 +184,6 @@ function displayWeather(data) {
     document.querySelector(".city").innerText = cityname + ",";
     document.querySelector(".country").innerText = country;
     document.querySelector(".description").innerText = description;
-    document.querySelector("#currentdate").innerText = current_date;
     document.querySelector("#current_temp").innerText = Math.ceil(current_temp) ;
     document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
     document.querySelector("#currentday").innerText = current_day+",";
@@ -164,78 +191,157 @@ function displayWeather(data) {
     document.querySelector("#currentdate").innerText = currentdate;
     
     
-   console.log(location);
+   
     
     
     /*----------------------------------------------------------------------------------*/
 
-    /*-------------------------------------------------------------------------------------*/
-    /*To display daily forecase data for 7 days*/
-    /*--------------------------------------------------------------------------------------*/
-    var days = document.querySelectorAll(".days");
-    var classes = document.querySelectorAll(".icon");
-    var dates = document.querySelectorAll(".date");
-    var desc = document.querySelectorAll(".desc");
-    var dailytemp_max = document.querySelectorAll(".temp_max");
-    var dailytemp_min = document.querySelectorAll(".temp_min");
-    for (let i = 0; i <= days.length - 1; i++) {
-        let imageicons = (classes[i]);
-        let setdate = dates[i];
-        let setdesc = desc[i];
-
-        let settemp_max = dailytemp_max[i];
-        let settemp_min = dailytemp_min[i];
-        if (daily_description[i + 1].includes("clouds")) {
-
-            imageicons.src = "icons/overcast clouds.svg";
-        }
-        if (daily_description[i + 1].includes("rain")) {
-
-            imageicons.src = "icons/rain.svg";
-        }
-        if (daily_description[i + 1].includes("snow")) {
-            imageicons.src = "icons/snow.svg";
-        }
-        if (daily_description[i + 1].includes("hail")) {
-            imageicons.src = "icons/hail.svg";
-        }
-        if (daily_description[i + 1].includes("haze")) {
-            imageicons.src = "icons/haze.svg";
-        }
-        if (daily_description[i + 1].includes("clear")) {
-            imageicons.src = "icons/clear sky.svg";
-        }
-        if (daily_description[i + 1].includes("smoke")) {
-            imageicons.src = "icons/smoke.svg";
-        }
-        if (daily_description[i + 1].includes("fog")) {
-            imageicons.src = "icons/fog.svg";
-        }
-        if (daily_description[i + 1].includes("thunderstorms")) {
-            imageicons.src = "icons/thunderstorms.svg";
-        }
-
-
-
-
-
-
-        setdate.innerText = weekday[date[i + 1].getDay()]
-        setdesc.innerText = daily_description[i + 1];
-        settemp_min.innerText = Math.ceil(temp_min[i + 1]) + "\xB0C";
-        settemp_max.innerText = Math.ceil(temp_max[i + 1]) + "\xB0C";
-
-        imageicons.src="https://openweathermap.org/img/wn/"+daily_icon[i+1]+".png";
     
 
+      
+
+}
+/*-------------------------------------------------------------------------------------*/
+    /*To display daily forecase data for 7 days*/
+    /*--------------------------------------------------------------------------------------*/
+   function iconurl(description)
+   {
+    if(description.includes("clouds"))
+    {
+        return("icons/overcastclouds.svg");
+    }
+    if(description.includes("haze"))
+    {
+        return("icons/haze.svg");
+    }
+    if(description.includes("mist"))
+    {
+        return("icons/mist.svg");
+    }
+    if(description.includes("rain"))
+    {
+        return("icons/rain.svg");
+    }
+    if(description.includes("snow"))
+    {
+        return("icons/snow.svg");
+    }
+    if(description.includes("clear"))
+    {
+        return("icons/clearsky.svg");
     }
 
+
+   }
+
+
+function weeklydata(data){
+    
+    const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]; 
+    for (let i = 0; i <=data.length - 1; i++) {
+        daily_description[i] = dailydata[i].weather[0].description;
+        daily_icon[i] = dailydata[i].weather[0].icon
+        date[i] = new Date(dailydata[i].dt * 1000).getDay();
+        temp_max[i] = dailydata[i].temp.max;
+        temp_min[i] = dailydata[i].temp.min;
+        
+     
+    }
+    const days=[
+        {
+            id:'day1',
+            icon:iconurl(daily_description[1]),
+            date:weekday[date[1]],
+            min:temp_min[1],
+            max:temp_max[1]
+        },
+        {
+            id:'day2',
+            icon:iconurl(daily_description[2]),
+            date:weekday[date[2]],
+            min:temp_min[2],
+            max:temp_max[2]
+        },
+        {
+            id:'day3',
+            icon:iconurl(daily_description[3]),
+            date:weekday[date[3]],
+            min:temp_min[3],
+            max:temp_max[3]
+        },
+        {
+            id:'day4',
+            icon:iconurl(daily_description[4]),
+            date:weekday[date[4]],
+            min:temp_min[4],
+            max:temp_max[4]
+        },
+        {
+            id:'day5',
+            icon:iconurl(daily_description[5]),
+            date:weekday[date[5]],
+            min:temp_min[5],
+            max:temp_max[5]
+        }
+        
+    
+    ]
+    console.log(days)
+    let daily=days.map(function (item)
+    {
+        return ` <div class="weekly">
+        <div class="day">${item.date}</div> 
+        <img src=${item.icon}  alt="">
+        <div class="tempmin">${item.min}</div>
+        <div class="tempmax">${item.max}</div>
+        </div>`
+        ;
+    }
+    );
+    daily=daily.join(" ");
+    
+    const weeklyweather=document.querySelector(".weeklyweather");
+    weeklyweather.innerHTML=daily;
+    
+    
+  
 }
 
 
-/*-----------------------------------------------------------------------------------*/
-window.addEventListener("DOMContentLoaded",function(){
+   
+        
+        
+
+    
+    
+ 
+     
+    
    
 
-    });
+
+
+
+   
+
+
+
+
+
+    
+   
+
+/*-----------------------------------------------------------------------------------*/
+
+
+    
+        
+   
+        
+        
+    
+ 
+  
+
+    
 
